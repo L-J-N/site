69df7ca92d9bad928372f3f2501985b94a419b3a
nilar <nicolas.larche@rca.fr>
2016-10-13 14:34:08 +0200

    Add test

--- src/components/NotFound/index.test.js
+++ src/components/NotFound/index.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './index';
+import NotFound from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App><div>app</div></App>, div);
+  ReactDOM.render(<NotFound />, div);
 });
