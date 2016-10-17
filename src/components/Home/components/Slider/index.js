bc35536d776b29aef7e259f7418e1343a24e1188
nilar <nicolas.larche@rca.fr>
2016-10-17 14:34:03 +0200

    Add gsap for transition

--- src/components/Home/components/Slider/index.js
+++ src/components/Home/components/Slider/index.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-transition-group';
+import TransitionGroup from 'react-addons-transition-group';
+import { TweenLite } from "gsap";
+import { findDOMNode } from 'react-dom';
+
 
 import './index.css';
 
-const Carroussel = (props) => (
-  <ReactCSSTransitionGroup
-    transitionName="slide"
-    transitionEnterTimeout={500}
-    transitionLeaveTimeout={500} >
-    <img key={props.index}
-         className={props.class}
-         src={props.url} alt="" />
-  </ReactCSSTransitionGroup>
-);
+class Carroussel extends React.Component {
+  componentWillEnter(callback) {
+    const el = findDOMNode(this);
+    TweenLite.fromTo(el, 0.5, { x: -400, opacity: 1 }, { x: 0, opacity: 1 ,onComplete: callback });
+  }
+  componentWillLeave(callback) {
+    const el = findDOMNode(this);
+    TweenLite.fromTo(el, 0.5, { x:0, opacity: 1 }, { x: 400, opacity: 0, onComplete: callback });
+  }
+  render() {
+    return (
+      <figure className="slider-figure image is-128x128">
+        <img key={this.props.index} className={this.props.class}  src={this.props.url} alt="" />
+      </figure>
+    );
+  }
+}
 
 export default class Slider extends Component {
   constructor() {
@@ -29,13 +39,15 @@ export default class Slider extends Component {
   precedent() {
     let { index } = this.state;
     this.setState({
-      index: parseInt(index, 10) === 0 ? 5 : --index
+      index: parseInt(index, 10) === 0 ? 5 : --index,
+      direction: 'left'
     });
   }
   suivant() {
     let { index } = this.state;
     this.setState({
-      index: parseInt(index, 10) === 5 ? 0 : ++index
+      index: parseInt(index, 10) === 5 ? 0 : ++index,
+      direction: 'right'
     });
   }
   selectRadio(e) {
@@ -45,10 +57,11 @@ export default class Slider extends Component {
   }
   render() {
 
-    const image  = {
-      index : this.state.index,
+    const image = {
+      index: this.state.index,
       url: "http://placehold.it/128x128?text=" + this.state.index,
-      class: "border-" + this.state.index
+      class: "border-" + this.state.index,
+      direction : this.state.direction
     };
 
     return (
@@ -57,9 +70,9 @@ export default class Slider extends Component {
           <i className="fa fa-arrow-left"></i>
         </a>
         <div className="slider-content">
-          <figure className="slider-figure image is-128x128">
-              <Carroussel {...image} />
-          </figure>
+          <TransitionGroup style={{display: 'flex'}}>
+            <Carroussel key={this.state.index} {...image}/>
+          </TransitionGroup>
           <div>
             {
               [0, 1, 2, 3, 4, 5].map((i) => {
@@ -72,7 +85,6 @@ export default class Slider extends Component {
                 );
               })
             }
-
           </div>
         </div>
         <a className="button is-large button-suivant" onClick={this.suivant} >
