5344095c5036222ec60c3ac0534ee1d3782cf2d4
nilar <nicolas.larche@rca.fr>
2016-10-17 16:08:16 +0200

    Projet du mois

--- src/components/Home/ProjetDuMois/index.js
+++ src/components/Home/ProjetDuMois/index.js
@@ -6,7 +6,23 @@ export default class ProjetDuMois extends Component {
   render() {
     return (
       <div>
-       ProjetDuMois
+        <h1>Projet Du Mois</h1>
+        <div>
+          <img src="http://placehold.it/512x512?text=projet du mois" alt="projet du mois" />
+          <div>
+            <span>le nom</span>
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Pellentesque risus mi, tempus quis placerat ut, porta nec
+                  nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam
+                  gravida purus diam, et dictum felis venenatis efficitur.
+                  Aenean ac eleifend lacus, in mollis lectus. Donec sodales,
+                  arcu et sollicitudin porttitor, tortor urna tempor ligula,
+                  id porttitor mi magna a neque. Donec dui urna, vehicula et
+                  sem eget, facilisis sodales sem.
+              </div>
+          </div>
+        </div>
       </div>
     );
   }
