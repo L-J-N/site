89ba56a6f2a3fec1711d998fb44b7e1c487f93e9
nilar <nicolas.larche@rca.fr>
2017-02-15 15:16:08 +0100

    ajout wysiwyg

--- src/App/index.js
+++ src/App/index.js
@@ -19,7 +19,6 @@ class Site extends Component {
         </div>
         <Footer />
       </App>);
-
   }
 }
 
--- src/pages/NouveauProjet/index.js
+++ src/pages/NouveauProjet/index.js
@@ -1,7 +1,73 @@
 import React, { Component } from 'react';
 
+import { convertToRaw } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import draftToMarkdown from 'draftjs-to-markdown';
+
+import { Editor } from 'react-draft-wysiwyg';
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+import {
+  Box,
+  Button,
+} from '../../components';
+
 export default class NouveauProjet extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: null
+    };
+    this.onChange = this.onChange.bind(this);
+    this.toHtml = this.toHtml.bind(this);
+    this.toMd = this.toMd.bind(this);
+    this.toJson = this.toJson.bind(this);
+  }
+
+  onChange(editorState) {
+    this.setState({
+      editorState
+    });
+  }
+
+  toHtml() {
+    const value = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()));
+    console.log(value);
+  }
+
+  toMd() {
+    const value = draftToMarkdown(convertToRaw(this.state.editorState.getCurrentContent()));
+    console.log(value);
+  }
+
+  toJson() {
+    const value = JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()));
+    console.log(value);
+  }
+
   render() {
-    return <div>nouveau</div>;
+    return (
+      <Box colorIndex="light-2" pad="large">
+        <Editor
+          toolbarOnFocus
+          editorState={this.state.editorState}
+          onEditorStateChange={this.onChange}
+          toolbar={this.toolbar}
+          placeholder="Entrez votre texte ici"
+          />
+        <Box direction="row" colorIndex="light-1" pad="large">
+          <Button label='Html'
+            primary={true}
+            onClick={this.toHtml} />
+          <Button label='Markdown'
+            primary={true}
+            onClick={this.toMd} />
+          <Button label='json'
+            primary={true}
+            onClick={this.toJson} />
+          </Box>
+        </Box>
+        );
   }
 }
